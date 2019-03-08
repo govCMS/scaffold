87cff512e9ce3bd0c43c684a6b3f9494806308e5
Stuart Rowlands <stuart@firecannon.com>
2019-03-08 17:17:52 +1000

    Drush8 alias support.

---
+++ .docker/aliases.drushrc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Alias for GovCMS prod environment only accessible within lagoon.
+ *
+ */
+
+ if (getenv('LAGOON_PROJECT')) {
+  $aliases["govcms-prod"] = array (
+    "root" => "/app",
+    "remote-host" => "ssh.lagoon.svc",
+    "remote-user" => getenv('LAGOON_PROJECT') . "-master",
+    "ssh-options" => "-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2020 -F /dev/null",
+  );
+ }
