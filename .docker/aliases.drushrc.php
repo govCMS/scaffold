c3df25f27515e5d2d5cdeb73ff234d7acfaa7ac7
Stuart Rowlands <stuart@firecannon.com>
2019-03-11 08:26:52 +1000

    Resolve govcms-prod drush alias.

--- .docker/aliases.drushrc.php
+++ .docker/aliases.drushrc.php
@@ -8,7 +8,7 @@
 
  if (getenv('LAGOON_PROJECT')) {
   $aliases["govcms-prod"] = array (
-    "root" => "/app",
+    "root" => "/app/web",
     "remote-host" => "ssh.lagoon.svc",
     "remote-user" => getenv('LAGOON_PROJECT') . "-master",
     "ssh-options" => "-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2020 -F /dev/null",
