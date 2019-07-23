dc61834488c6c8a6e7ed014518c67b4d49885d50
sime <simon@hobbs.id.au>
2019-07-23 20:24:47 +1000

    In search of a cleaner paas with some docs.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,3 +1,11 @@
+##
+# `php` image
+#
+# This image is derived from php-fpm, which lacks tools to build Drupal.
+# The files are therefore copied from the `cli` container (Dockerfile.cli) which is built first.
+#
+
+# CLI_IMAGE set in &lagoon-project in docker-compose.yml
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE} as cli
 
