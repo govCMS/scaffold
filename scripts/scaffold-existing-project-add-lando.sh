8a446497975c87f77f48907a4cac07befb2cc73c
Joshua Graham <joshua.graham@au.ey.com>
2026-04-13 14:32:20 +1000

    Fix regex for project name detection.

--- scripts/scaffold-existing-project-add-lando.sh
+++ scripts/scaffold-existing-project-add-lando.sh
@@ -10,7 +10,7 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 ################## TODO "my-project" is "my" instead?
-GOVCMS_NAME=$(grep -oP 'http://\K\w+' docker-compose.yml | head -n 1)
+GOVCMS_NAME=$(grep -oP 'http:\/\/\K[^\n]+(?=\.docker\.amazee\.io)' docker-compose.yml | head -n 1)
 GOVCMS_VERSION=$(grep -oP 'GOVCMS_IMAGE_VERSION:-\K\w+' docker-compose.yml | head -n 1)
 grep -q '^  solr:$' docker-compose.yml
 HAS_SOLR=$?
